8afe96a834bbc2b041ed6a9b2aafc8f8b92ae956
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2024-12-25 14:38:07 +0530

    url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatify.onrender.com/", {
+			const socket = io("https://chatify-def4.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
